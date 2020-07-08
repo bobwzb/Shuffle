d8445f217878eb9d73082b76e52f549eb8cff6f4
bobwzb <42678607+bobwzb@users.noreply.github.com>
2020-07-08 01:32:57 -0700

    initail project

---
+++ Shuffle/Shuffle/Shuffle.cpp
@@ -0,0 +1,20 @@
+﻿
+
+#include "pch.h"
+#include <iostream>
+#include <vector>
+using namespace std;
+void FisherYatesShuffle(vector<int>& pool) {
+
+}
+int main()
+{
+	vector<int> pool(54);
+	for (int i = 0; i < 54; i++) {
+		pool[i] = i+1;
+		if (i + 1 < 10) cout << pool[i] << "  ";
+		else cout << pool[i] << " ";
+		if ((i + 1) % 9 == 0) cout << endl;
+	}
+}
+
