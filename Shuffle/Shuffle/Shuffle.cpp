04930e462e572df3818f8f7c68c57fa93dd786a5
bobwzb <42678607+bobwzb@users.noreply.github.com>
2020-07-10 02:33:24 -0700

    add test func

--- Shuffle/Shuffle/Shuffle.cpp
+++ Shuffle/Shuffle/Shuffle.cpp
@@ -5,8 +5,8 @@
 #include <vector>
 using namespace std;
 void FisherYatesShuffle(vector<int>& pool) {
-	for (int i = 53; i > 1; i--) {
-		int j = i - 1;
+	for (int i = 53; i >=0; i--) {
+		int j = i + 1;
 		j = rand() % j;
 		swap(pool[i], pool[j]);
 	}
@@ -14,9 +14,16 @@ void FisherYatesShuffle(vector<int>& pool) {
 void ReservoirSampling(vector<int>& pool) {
 
 }
+void test(vector<int>& pool, vector<int>& cnt) {
+	for (int i = 0; i < 30000; i++) {
+		FisherYatesShuffle(pool);
+		cnt[pool[0] - 1]++;
+	}
+}
 int main()
 {
 	vector<int> pool(54);
+	vector<int> cnt(54,0);
 	cout << endl << "Original:" << endl;
 	for (int i = 0; i < 54; i++) {
 		pool[i] = i+1;
@@ -24,11 +31,18 @@ int main()
 		else cout << pool[i] << " ";
 		if ((i + 1) % 9 == 0) cout << endl;
 	}
-	//FisherYatesShuffle(pool);
+	FisherYatesShuffle(pool);
 	cout <<endl<< "After Shuffle:" << endl;
 	for (int i = 0; i < 54; i++) {
 		if (pool[i] < 10) cout << pool[i] << "  ";
 		else cout << pool[i] << " ";
+		if ((i + 1) % 9 == 0) cout << endl;
+	}
+	test(pool, cnt);
+	cout << endl << "Test Result:" << endl;
+	for (int i = 0; i < 54; i++) {
+		if (cnt[i] < 10) cout << cnt[i] << "  ";
+		else cout << cnt[i] << " ";
 		if ((i + 1) % 9 == 0) cout << endl;
 	}
 }
